ed7871c0b4a9436245f1673e61dfcda91a76f7f9
Ning Miao <nmiao@ucdavis.edu>
2021-10-14 23:16:00 -0700

    Update: terminal installing section finished

--- server_setup.sh
+++ server_setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# This is the section of install terminal
 # First we need to check and install zsh and oh my zsh
 if ! [ -x  "$(command -v zsh)" ]; then
     echo 'zsh is not installed.' >&2
 else
     echo 'zsh is installed, skip.' >&1
 fi
+
+# Using official script to install oh-my-zsh will stop if already installed
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Setting up zshrc
+sed -i 's|ZSH_THEME=.*|ZSH_THEME="agnoster"|' ~/.zshrc
+line=$(sed -n '/plugins/=' ~/.zshrc | sed -n "2"p)
+sed -i "${line}s|plugins=.*|plugins=(git extract zsh-syntax-highlighting zsh-autosuggestions)|" ~/.zshrc
+source ~/.zshrc
