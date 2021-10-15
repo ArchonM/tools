14f0d9f76f9c44a27587d95886a008abf517f835
Ning Miao <nmiao@ucdavis.edu>
2021-10-14 23:17:14 -0700

    Fix: install zsh after verification

--- server_setup.sh
+++ server_setup.sh
@@ -4,6 +4,7 @@
 # First we need to check and install zsh and oh my zsh
 if ! [ -x  "$(command -v zsh)" ]; then
     echo 'zsh is not installed.' >&2
+    apt-get install -y zsh
 else
     echo 'zsh is installed, skip.' >&1
 fi
