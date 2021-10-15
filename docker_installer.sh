1af58013632a27b6cf6c3966e14d88406b43a5f2
Ning Miao <nmiao@ucdavis.edu>
2021-10-14 23:35:52 -0700

    Completed: script that insatlls Docker

---
+++ docker_installer.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+# This script installs docker for ubuntu only
+# Just incase, remove the old versions
+apt-get remove docker docker-engine docker.io containerd runc
+# Now, Start installing the lastest version
+apt-get update
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
