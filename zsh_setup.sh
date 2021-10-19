4d3a97e375ad0773336899e39a633afcc403bc1b
Ning Miao <nmiao@ucdavis.edu>
2021-10-19 01:53:15 -0700

    Fix: apply zsh rules after installing plugins

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -7,3 +7,5 @@ source ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+source ~/.zshrc
